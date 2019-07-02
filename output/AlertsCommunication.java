098eb731aec0c74fae12252ceab6d163a3fbfd81
Healthedata1 <ehaas@healthedatainc.com>
2019-07-02 10:27:59 -0700

    initial commit

---
+++ output/AlertsCommunication.java
@@ -0,0 +1,7 @@
+package http://hl7.org/fhir/us/davinci-alerts/ImplementationGuide/hl7.fhir.us.davinci-alerts-0.0.0;
+
+import org.hl7.fhir.r5.model.ProfilingWrapper;
+
+public class AlertsCommunication {
+
+}
