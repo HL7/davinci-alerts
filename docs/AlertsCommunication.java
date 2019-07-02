cd2cce0cca7f976e5cd4aaf31a4833e1f1f83af7
Healthedata1 <ehaas@healthedatainc.com>
2019-07-02 10:34:41 -0700

    remove docs dir

--- docs/AlertsCommunication.java
+++
@@ -1,7 +0,0 @@
-package http://hl7.org/fhir/us/davinci-alerts/ImplementationGuide/hl7.fhir.us.davinci-alerts-0.0.0;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class AlertsCommunication {
-
-}
