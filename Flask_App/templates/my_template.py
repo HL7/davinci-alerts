fd8f0ec00190082e6bb763fd3d26028054dd2384
Healthedata1 <ehaas@healthedatainc.com>
2019-12-20 06:36:03 -0800

    update title from CI to Ballot

--- Flask_App/templates/my_template.py
+++ Flask_App/templates/my_template.py
@@ -3,11 +3,9 @@ def foo(*args,**kwargs):
     return(
     {
     'foo':'bar',
-    'param' : param1
+    'param' : kwargs['param1']
     }
     )
-
-param1='bar1'
 
 if __name__ == "__main__":
     print(foo(param1='bar1'))
