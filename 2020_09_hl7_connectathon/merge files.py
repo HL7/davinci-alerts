9b326a2d1a1591a2d8256645fbded78eb72b5a7b
Healthedata1 <ehaas@healthedatainc.com>
2020-08-19 16:35:57 -0700

    add test data

--- 2020_09_hl7_connectathon/merge files.py
+++ 2020_09_hl7_connectathon/merge files.py
@@ -1,20 +1,33 @@
-from json import load
+"""
+Script to update element in bundles - in this case the system in Encounter type and Condition code
+"""
+from json import load, dumps
+
 
 map={}
-file='2020_05_hl7_connectathon/Synthea_Alert_Test_Data/fhir/admit_notify-2.json'
+file='2020_09_hl7_connectathon/Synthea_Alert_Test_Data/fhir/transfer_notify-100.json'
 with open(file) as f:
     b = load(f)
 
 print(b['resourceType'])
 
+for e in b['entry']:
 
-r_index = [e['fullUrl'] for e in b['entry'] ]
+    try:
+        print(e['resource']['type'][0]['coding'][0]['display'])
+        display = e['resource']['type'][0]['coding'][0]['display']
+        system = e['resource']['type'][0]['coding'][0]['system']
+    except KeyError:
+        pass
+    else:
+        if display == 'Examplotomy':
+            print('change ' + system + ' to http://example.org/fhir/conditions')
+            e['resource']['type'][0]['coding'][0]['system'] = 'http://example.org/fhir/conditions'
+        elif display == 'Examplotomy Encounter':
+            print('change ' + system + ' to http://example.org/fhir/encounters')
+            e['resource']['type'][0]['coding'][0]['system'] = 'http://example.org/fhir/encounters'
+print(type(b))
+print(dumps(b))
 
-print(r_index)
-print(len(r_index))
-
-for e,i in enumerate(b.entry):
-
-    map[i] = r_index(i.fullUrl)
-
-print map
+with open(file, 'w') as f:
+    f.write(dumps(b))
